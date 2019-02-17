f418cd700e26a3062a79b6b55f68766568925ccb
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-02-17 16:19:04 -0500

    test urlsafe-base64

--- tests/run.scm
+++ tests/run.scm
@@ -1,5 +1,6 @@
 (include "../constant-time-equal.scm")
-(import constant-time-equal test)
+(include "../urlsafe-base64.scm")
+(import constant-time-equal jwt test urlsafe-base64)
 
 (test-begin "jwt")
 
@@ -23,6 +24,21 @@
     (constant-time-equal?
       "abcdefghijklmnopqrstuvwxyz"
       "abcdefghijk")))
+
+(test-group "urlsafe-base64"
+  (test "removes padding"
+        "TGV0IHVzIHRyeSB0byB0ZWFjaCBnZW5lcm9zaXR5IGFuZCBhbHRydWlzbSwgYmVjYXVzZSB3ZSBhcmUgYm9ybiBzZWxmaXNoLg"
+        (urlsafe-base64-encode
+          "Let us try to teach generosity and altruism, because we are born selfish."))
+  (test "substitutes unsafe characters"
+        "BgYGQkJCAQIDBN6tvu__"
+        (urlsafe-base64-encode
+          "\x06\x06\x06\x42\x42\x42\x01\x02\x03\x04\xDE\xAD\xBE\xEF\xFF"))
+  (test "invertible"
+        "The genes are master programmers, and they are programming for their lives."
+        (urlsafe-base64-decode
+          (urlsafe-base64-encode
+            "The genes are master programmers, and they are programming for their lives."))))
 
 (test-end "jwt")
 (test-exit)
