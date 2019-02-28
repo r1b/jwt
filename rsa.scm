2607b39167d4c8295d3d56bde5e7d0abcbf6451b
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-02-28 16:41:24 -0500

    use EVP_DigestSign*

--- rsa.scm
+++ rsa.scm
@@ -28,16 +28,16 @@
          (signature (make-blob signature-length)))
     (begin
       ((foreign-lambda void "EVP_MD_CTX_init" c-pointer) ctx)
-      ((foreign-lambda int "EVP_SignInit"
-                       c-pointer c-pointer)
-       ctx type)
-      ((foreign-lambda int "EVP_SignUpdate"
+      ((foreign-lambda int "EVP_DigestSignInit"
+                       c-pointer c-pointer c-pointer c-pointer c-pointer)
+       ctx #f type #f pkey)
+      ((foreign-lambda int "EVP_DigestSignUpdate"
                        c-pointer scheme-pointer unsigned-int)
        ctx message (blob-size message))
-      ((foreign-lambda* int ((c-pointer ctx) (blob sig) (unsigned-int s) (c-pointer pkey))
-         "C_return(EVP_SignFinal(ctx, sig, &s, pkey));")
-       ctx signature signature-length pkey)
-      ((foreign-lambda void "EVP_MD_CTX_destroy" c-pointer) ctx) ; XXX I'm assuming this also frees the EVP_MD in `type`
+      ((foreign-lambda* int ((c-pointer ctx) (blob sig) (size_t siglen))
+         "C_return(EVP_DigestSignFinal(ctx, sig, &siglen));")
+       ctx signature signature-length)
+      ((foreign-lambda void "EVP_MD_CTX_destroy" c-pointer) ctx)
       ((foreign-lambda void "EVP_PKEY_free" c-pointer) pkey)
       ((foreign-lambda void "BIO_free" c-pointer) bio)
       (blob->string signature))))
