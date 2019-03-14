466af181bc763986f12c648dd61741420c7a2487
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-03-13 23:33:14 -0400

    invertible tests

--- tests/run.scm
+++ tests/run.scm
@@ -19,30 +19,21 @@
             "The genes are master programmers, and they are programming for their lives."))))
 
 (test-group "codecs"
-  (test "encode empty hmac payload"
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.e30.DMCAvRgzrcf5w0Z879BsqzcrnDFKBY_GN6c3qKOUFtQ"
-        (jwt-encode '() "secret"))
-
-  (test "decode empty hmac payload"
+  (test "hs256"
         '()
-        (jwt-decode "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.e30.DMCAvRgzrcf5w0Z879BsqzcrnDFKBY_GN6c3qKOUFtQ" "secret"))
+        (jwt-decode (jwt-encode '() "secret") "secret"))
 
-  (test "encode empty rsa payload"
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.e30.D_jZijCIsgJE_hd_HnF8-3If11ExB0RTVgVGaNetM1CSSDtMG47j_O6r2q9J2WoQ1hCuPzF_v2N9YmmCmTKQw1xxU_rphFYOs9SIyp-80CX4FinzZYamgsuRKsHo4AecRduicQn4oX-sHfLjuD56ZBUvc2K5Y4t4f0Lp4mj02i9Qip-xvQzLKEd43Wnkl84eX_y_JrqxNCjX8vcV8Wlj2PQm9h0BDtwuKdFEaR5fpKOnD4oBMs56g7QhRGEc9U-UDPWqeDAWAdMht6u_vjvKQQZ6gvVGpnZQt-S6vOMwz2J1Q1HMWqJBu07ShDByIDpxPAaKhkf0v6ZoPtigDY1ioQ"
-        (jwt-encode '() (read-string #f (open-input-file "rsa-private.pem")) "RS256"))
-
-
-  (test "decode empty rsa payload"
+  (test "rs256"
         '()
-        (jwt-decode "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.e30.D_jZijCIsgJE_hd_HnF8-3If11ExB0RTVgVGaNetM1CSSDtMG47j_O6r2q9J2WoQ1hCuPzF_v2N9YmmCmTKQw1xxU_rphFYOs9SIyp-80CX4FinzZYamgsuRKsHo4AecRduicQn4oX-sHfLjuD56ZBUvc2K5Y4t4f0Lp4mj02i9Qip-xvQzLKEd43Wnkl84eX_y_JrqxNCjX8vcV8Wlj2PQm9h0BDtwuKdFEaR5fpKOnD4oBMs56g7QhRGEc9U-UDPWqeDAWAdMht6u_vjvKQQZ6gvVGpnZQt-S6vOMwz2J1Q1HMWqJBu07ShDByIDpxPAaKhkf0v6ZoPtigDY1ioQ" (read-string #f (open-input-file "rsa-public.pub")) "RS256"))
+        (jwt-decode
+          (jwt-encode '() (read-string #f (open-input-file "rsa-private.pem")))
+          (read-string #f (open-input-file "rsa-private.pem"))))
 
-  ; XXX DSA is not stable - how to test encode?
-
-  ; XXX This seems to work but jwt.io is not convinced?
-
-  (test "decode empty ecdsa payload"
+  (test "es256"
         '()
-        (jwt-decode "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.e30.MEYCIQChJplQ9YwNBkjRU-EbkcoQlun1_JeusIKqRUrbQuA3aQIhAKrHC55RJNd44dH_I0HCs9iIkv4kfTqhnHbssU8_NElQ" (read-string #f (open-input-file "ecdsa-public.pub")) "ES256"))
+        (jwt-decode
+          (jwt-encode '() (read-string #f (open-input-file "ecdsa-private.pem")))
+          (read-string #f (open-input-file "ecdsa-private.pem"))))
 
   (test-error
     "decode unexpected algorithm"
